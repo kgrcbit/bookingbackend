b496a24c3536df6073b1b344cdc2b1601c805712
kgrcbit <64593708+kgrcbit@users.noreply.github.com>
2024-07-29 21:35:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const path = require('path');
 
 const app = express()
 const mongoose  = require('mongoose')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 const MONGOURI='mongodb+srv://sportscbit:wZokJ2Ug0coojB8J@sport-cbit.79n6t5u.mongodb.net/mydb?retryWrites=true&w=majority&appName=sport-cbit'
 //const {MONGOURI} = require('./config/keys')
+/ Serve static files from the frontend build directory
+app.use(express.static(path.join(__dirname, 'bookingfrontend', 'build')));
 
+// Handle all other requests by serving the frontend's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'bookingfrontend', 'build', 'index.html'));
+});
 
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
@@ -36,13 +42,7 @@ app.use(require('./routes/bookings'))
 app.use(require('./routes/user'))
 
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
